64010032276884a131e2f6579ac23e91d9e2cd15
wangjinshen <jswangg@isoftstone.com>
2020-11-23 20:52:52 +0800

    deploy

---
+++ assets/js/9.b5c52c70.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{59:function(t,e,n){"use strict";n.r(e);var s=n(0),o=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("homeImg"),e("my-footer")],1)}),[],!1,null,null,null);e.default=o.exports}}]);
